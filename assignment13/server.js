9f7acdf158ada890978e8ad15eb838805aa2001d
Aarti Dalvi <aarti.dalvi09@gmail.com>
2019-12-22 15:29:14 +0500

    connect to mongodb cluster

--- assignment13/server.js
+++ assignment13/server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+
 const port = 5000;
 
-app.listen(port, () => {
-    console.log('Server running on port' + port);
+const mongoDBpath = 'mongodb+srv://aartidalvi:mongodbpwd@cluster0-ne0fo.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect (mongoDBpath, { useUnifiedTopology: true , useNewUrlParser: true } ,(error) => {
+    console.log("Error connecting to the databse: " + error);
+})
+
+var server = app.listen(port, () => {
+    console.log('Server running on port: ' + port);
 });
