2d37d5c89df2921d1a6e3a942f8e1d7db4ccdbfc
Aarti Dalvi <aarti.dalvi09@gmail.com>
2019-12-25 16:56:02 +0530

    Show default data with delete placeholder

--- assignment08/js/employee-management.js
+++ assignment08/js/employee-management.js
@@ -1,2 +1,42 @@
 /*eslint-env browser*/
 
+/* Initial 5 entries that are shown on page load */
+var employees = [
+    ['Aarti', 'Intern', 432],
+    ['Alex', 'Senior Manager', 342],
+    ['Carl', 'Software Engineer', 788],
+    ['Andrew', 'CEO', 566],
+    ['Neeta', 'Product Manager', 947],
+];
+
+var $ = function(id) {
+    return window.document.getElementById(id)
+};
+
+var employeesTable = $('employeesTable');
+
+function createTableCell(value) {
+    const cell = document.createElement('td');
+    const textNode = document.createTextNode(value);
+    cell.appendChild(textNode);
+    return cell;
+}
+
+function showRecords() {
+    const body = employeesTable.children[1];
+    body.innerHTML = '';
+    employees.map(employee => {
+        body.appendChild(getEmployeeRow(employee));
+    });
+}
+
+function getEmployeeRow(employee) {
+    const tr = document.createElement('tr');
+    tr.appendChild(createTableCell(employee[0]));
+    tr.appendChild(createTableCell(employee[1]));
+    tr.appendChild(createTableCell(employee[2]));
+    tr.appendChild(createTableCell('delete'));
+    return tr;
+}
+
+showRecords();
